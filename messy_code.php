4d36063db5cc72454a3a746b3195be85addd1542
insejn <piotrkonicki@gmail.com>
2025-08-14 19:54:16 +0200

    new indentation fix to be ignored in blame

--- messy_code.php
+++ messy_code.php
@@ -1,29 +1,29 @@
 <?php
 declare(strict_types=1);
 
-namespace	  Demo\Messy;
+namespace Demo\Messy;
 
-class	  Greeter  {
-      private string $name;
-       public function __construct( string $name	 =  'Fixed World' )
-      {
-          $this->name	 =  $name;  
-      }
-
-      public function   greet () : string
-     {
-           $time = (new \DateTimeImmutable())  ->format('H:i');
-           return  sprintf(  "Hello, %s! It is %s.",	 $this->name ,  $time );
-     }
+class Greeter {
+    private string $name;
+    public function __construct( string $name = 'Fixed World' )
+    {
+        $this->name = $name;  
+    }
+    
+    public function greet(): string
+    {
+        $time = (new \DateTimeImmutable())  ->format('H:i');
+        return  sprintf(  "Hello, %s! It is %s.",	 $this->name ,  $time );
+    }
 }
 
-function 	 messy_array(  ) : array
+function messy_array(): array
 {
-     return	 [
-            'tabs'	=>	"\t",
-            'spaces'  =>  '   ',
-           'mix'  =>  "\t   ",
-     ];
+    return [
+        'tabs'	=>	"\t",
+        'spaces'  =>  '   ',
+        'mix'  =>  "\t   ",
+    ];
 }
 
 ?>

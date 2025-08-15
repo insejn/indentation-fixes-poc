2b67403667e216cc74e7f2836c2fd2d3163b3e64
insejn <piotrkonicki@gmail.com>
2025-08-15 11:49:09 +0200

    reindented js

--- messy_script.js
+++ messy_script.js
@@ -2,22 +2,22 @@
 
 // Intentionally messy whitespace
 function	  makeCounter ( start = 0 ) {
-    let	 count   =   start;   
-      return function  ()  {
-          count	 =  	count + 1;  
-           return	  count;
+  let	 count   =   start;   
+  return function  ()  {
+    count	 =  	count + 1;  
+    return	  count;
   };
 }
 
 const	logger	 =  (msg) => {
-      if (typeof	console !==  	"undefined") {
-          console.log(  "[log]",	 msg );
-       }
+  if (typeof	console !==  	"undefined") {
+    console.log(  "[log]",	 msg );
+  }
 };
 
 const   counter   =   makeCounter(  5	);
 for (let i=0; i<3; 	 i++){
-     logger( `value: ${counter()}` );
+  logger( `value: ${counter()}` );
 }
 
 export	 default { counter, logger };
